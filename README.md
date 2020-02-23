0f17396a8dff90dde48598fcaffaa60a09cb1be1
Mohammd Hosein <mjalali1378@gmail.com>
2020-02-23 11:14:55 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Hello,
+This is a project for making persian twitter word cloud
+
+You have to do these two steps till your project would work:
+
+1- First of all you must install these libraries in your virtual environment:
+- tweepy==3.8.0
+- urllib3==1.25.8
+- wordcloud==1.6.0
+- wordcloud-fa==0.1.5
+- hazm==0.7.0
+- image==1.5.28
+- arabic-reshaper==2.0.15
+
+2- Second of all config your json file for twitter APIs
---
+++ wc.py
@@ -0,0 +1,80 @@
+import tweepy
+from wordcloud_fa import WordCloudFa
+from os import path
+from PIL import Image
+import numpy as np
+import re
+import json
+
+
+with open('config.json', 'r') as f:
+    dev = json.load(f)
+
+dev = dev['twitter']
+
+consKey = dev['consKey']  # put your Consumer API key
+consSecret = dev['consSecret'] # put your Consumer API secret key
+accessKey = dev['accessKey']
+accessSecret = dev['accessSecret']
+
+d = path.dirname(__file__)
+
+auth = tweepy.OAuthHandler(consumer_key=consKey, consumer_secret=consSecret)
+
+auth.set_access_token(accessKey, accessSecret)
+
+api = tweepy.API(auth)
+
+numberOfPages = 1
+numberOfTweetsPerPage = 200
+counter = 0
+cloud = ""
+txt = ""
+
+username = input("Enter the term to be searched")
+number_of_tweets = int(input("Enter the number of tweets"))
+
+if number_of_tweets > 200:
+    numberOfPages = int(number_of_tweets/200)
+else:
+    numberOfTweetsPerPage = number_of_tweets
+
+
+for i in range(numberOfPages):
+    tweets = api.user_timeline(screen_name=username, count=numberOfTweetsPerPage, page=i)
+    for each in tweets:
+        cloud = each.text
+        cloud = re.sub(r'[A-Za-z@]*', '', cloud)
+        counter += 1
+        txt = txt + ' ' + each.text
+        print(counter, cloud)
+
+
+txt = re.sub(r'[A-Za-z@]*', '', txt)
+
+twitter_mask = np.array(Image.open(path.join(d, "twitter-logo.jpg")))
+
+
+stop = ['می', 'من', 'که', 'به', 'رو', 'از', 'ولی', 'با', 'یه', 'این', 'نمی',
+        'هم', 'شد', 'ها', 'اما', 'تو', 'واقعا', 'در', 'نه', 'دارم', 'باید',
+        'آره', 'برای', 'تا', 'چه', 'کنم', 'بود', 'همه', 'دیگه', 'ای', 'اون',
+        'تی', 'حالا', 'بی', 'د', 'چرا', 'بابا', 'منم', 'کیه', 'توی', 'نیست', 'چی', 'باشه', 'که',
+        'بودم', 'می کنم', 'که', 'اینه', 'بهتر', 'داره', '']
+wc = WordCloudFa(
+    persian_normalize=True,
+    max_words=30000,
+    margin=0,
+    width=3000,
+    height=2500,
+    min_font_size=1,
+    max_font_size=3000,
+    background_color="black",
+    mask=twitter_mask,
+    include_numbers=False,
+    stopwords=stop
+
+).generate(txt)
+
+image = wc.to_image()
+image.show()
+image.save('twitter.png')
